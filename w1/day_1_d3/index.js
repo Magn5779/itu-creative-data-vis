be555621001ac1fbc6244d921feddb2308ef5a04
Magn5779 <mavr@itu.dk>
2024-07-08 18:24:23 +0200

    Hej magnus du skal nok klare det

--- w1/day_1_d3/index.js
+++ w1/day_1_d3/index.js
@@ -1,64 +1,49 @@
 //GOAL: can we set up a local coding environment, webpage and draw 1 shape on it?
 
-var width = 800;
-var height = 500;
-var xPos = width/2;
-var yPos = height/2;
+var width = 1000;
+var height = 1000;
+var xPos = width / 2;
+var yPos = height / 2;
 var rad = 10;
 
+var canvas = d3
+  .select("#myVis")
+  .append("svg")
+  .attr("width", "100%")
+  .attr("height", height)
+  .style("background-color", "purple")
+  .style("border", "5px solid black");
 
-var canvas = d3.select("#myVis")
-                .append("svg")
-                .attr("width", width)
-                .attr("height", height);
+var circleFromD3 = canvas
+  .append("circle")
+  .attr("cx", xPos)
+  .attr("cy", yPos)
+  .attr("r", 50)
+  .attr("fill", "white");
 
- var circleFromD3 = canvas.append("circle")
-        .attr("cx", xPos)
-        .attr("cy", yPos)
-        .attr("r", rad)
-        .attr("fill","red");
+var circle = canvas
+  .append("circle")
+  .attr("cx", 550)
+  .attr("cy", 100)
+  .attr("r", 50)
+  .attr("fill", "white");
 
+var circle2 = canvas
+  .append("circle")
+  .attr("cx", 250)
+  .attr("cy", 100)
+  .attr("r", 50)
+  .attr("fill", "white")
+  .attr("stroke", "black");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var square = canvas
+  .append("rect")
+  .attr("x", 400)
+  .attr("y", 400)
+  .attr("width", 200)
+  .attr("height", 300)
+  .attr("fill", "black")
+  .attr("stroke", "white");
 // var w = 500;
 // var h = 500;
 // var rad = 20;
